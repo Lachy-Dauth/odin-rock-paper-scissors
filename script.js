ff89ae46bf3aa6fcda863be648d549e38404cf12
Lachlan Dauth <lachlandauth@outlook.com>
2022-05-31 16:46:55 +0800

    Finished code for asingle round

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ const opts = ["Rock", "Paper", "Scissors"];
 function computerPlay(){
   return opts[Math.floor(Math.random()*opts.length)];
 }
+
+function playRound(playerSelection, comupterSelection){
+  // matches the selections to a number
+  let simpPlayer = validInputs.indexOf(playerSelection.split("")[0].toLowerCase());
+  let simpComp = validInputs.indexOf(comupterSelection.split("")[0].toLowerCase());
+  if (simpPlayer == -1 || simpComp == -1) { // check if the inputs are invalid
+    return "The input was invalid";
+  }
+  switch (simpPlayer - simpComp) {
+    case 0: // if the numbers are the same it is a draw
+      return("It Was A Draw! " + opts[simpPlayer] + " Draws With " + opts[simpComp]);
+      break;
+    case 1: 
+    case -2:
+      return("You Win! " + opts[simpPlayer] + " Beats " + opts[simpComp]);
+      break;
+    case -1:
+    case 2:
+      return("You Lose! " + opts[simpPlayer] + " Loses To " + opts[simpComp]);
+      break;
+  }
+}
+
+console.log(playRound("Rock", computerPlay()));
